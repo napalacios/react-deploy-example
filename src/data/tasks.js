f78f698ebc36e08e56304f7c655783b372e6d0dc
nahuel <nahueel.palacios@gmail.com>
2023-07-29 23:40:02 -0300

    primer commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 1,
+        title: 'Mi primer tarea',
+        description: 'Mi primer tarea',
+    },
+    {
+        id: 2,
+        title: 'Mi segunda tarea',
+        description: 'Mi segunda tarea',
+    },
+    {
+        id: 3,
+        title: 'Mi tercera tarea',
+        description: 'Mi tercera tarea',
+    }
+]
